d2db9fca33e3be2547c4597c69d74f5104d0d7ae
Robert <robert.balicki@gmail.com>
2018-03-25 16:49:40 -0400

    shell out to du

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+// #[macro_use] extern crate quicli;
+// use quicli::prelude::*;
+
+fn main() {
+  use std::process::Command;
+
+  let output = if cfg!(target_os = "windows") {
+    Command::new("cmd")
+      .args(&["/C", "echo in theory this should work on Windows"])
+      .output()
+      .expect("failed to execute process")
+  } else {
+    Command::new("sh")
+      .arg("-c")
+      .arg("du -d 0 .")
+      .output()
+      .expect("failed to execute process")
+  };
+
+  let hello: Vec<u8> = output.stdout;
+  println!("{}", String::from_utf8(hello).unwrap())
+}
