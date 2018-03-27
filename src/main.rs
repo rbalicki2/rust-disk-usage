fbe3bea885e1042b1eb5db61014c4dfb0648bdc0
Robert <robert.balicki@gmail.com>
2018-03-26 23:59:36 -0400

    parallelize it!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 #[macro_use] extern crate quicli;
 use quicli::prelude::*;
+use rayon::iter::IntoParallelRefIterator;
+use rayon::iter::ParallelIterator;
+
+extern crate rayon;
+// use rayon::prelude::*;
 
 // mostly equivalent to
 // find ./src/ -type f -exec ls -l {} \; | awk '{sum += $5} END {print sum}'
@@ -15,16 +20,23 @@ struct DuArgs {
 
 fn get_folder_size(folder: &str) -> u64 {
   use std::fs;
+  // use std::path;
   let metadata = fs::metadata(folder).unwrap();
 
   if metadata.is_dir() {
     let paths = fs::read_dir(folder).unwrap();
     return paths
+      .map(|p| p.unwrap().path().to_str().unwrap().to_string())
+      .collect::<Vec<String>>()
+      // .into_iter()
+      .par_iter()
+      // .cloned()
+      // .into_iter()
       .map(|p| {
-        let p = p.unwrap();
-        get_folder_size(p.path().to_str().unwrap())
+        // let p = p.unwrap();
+        get_folder_size(&p)
       })
-      .fold(0, |acc, size| acc + size)
+      .reduce(|| 0, |acc, size| acc + size);
   }
   metadata.len()
 }
